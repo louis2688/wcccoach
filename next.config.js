f037246c668f132ec8de66173285cc5b6d8ff28d
louis <louismadrigal26@gmail.com>
2023-08-27 03:53:15 +0800

    Optimized images

--- next.config.js
+++ next.config.js
@@ -23,7 +23,7 @@ module.exports = withPWA(
     withReactSvg({
         reactStrictMode: false,
         images: {
-            unoptimized: true,
+            // unoptimized: true,
         },
         webpack(config) {
             return config;
